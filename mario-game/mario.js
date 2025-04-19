a82b9ceb3f7a8bb0ca3ea68b9a2e5a7af94e48d3
hosam3574 <hossamtradat@gmail.com>
2025-04-19 15:45:43 +0300

    mariojs

---
+++ mario-game/mario.js
@@ -0,0 +1,37 @@
+var mario =document.getElementById('mario');
+var left =0 ;
+var top1 =310;
+
+
+console.log('mario');
+function moveUp(){
+    console.log("up")
+    if(top1>=460){
+        return
+    }
+    top1= top1+10
+    mario.style.bottom=top1+'px'
+    console.log(top1)
+}
+
+function moveDown(){
+    console.log("down")
+    if(top1<=310){
+        return
+    }
+    top1= top1-10
+    mario.style.bottom=top1+'px'
+    console.log(top1)
+}
+
+function moveLeft(){
+    console.log("left")
+    left=left-10
+    mario.style.left=left+'px'
+}
+
+function moveRight(){
+    console.log("right")
+      left=left+10
+    mario.style.left=left+'px'
+}
